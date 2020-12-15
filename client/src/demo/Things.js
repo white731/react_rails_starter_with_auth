cb05e3d19e484dc7f9c2dfb9f87e9a51a04e6fda
Landon Whitesides <whitesideslandon@gmail.com>
2020-12-15 10:31:04 -0700

    set up axios call

--- client/src/demo/Things.js
+++ client/src/demo/Things.js
@@ -1,8 +1,55 @@
-import {Header} from "semantic-ui-react"
+import {Header, Segment, Dimmer, Loader} from "semantic-ui-react"
+import { useEffect, useState } from "react"
+import Axios from "axios"
 
 export default () => {
-  return (
-    <Header>Things</Header>
-  )
 
+  const [things, setThings] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+
+  useEffect(()=>{
+    getThings()
+  },[])
+
+  const getThings = async () => {
+    try { 
+      let res = await Axios.get("/api/things")
+      setThings(res.data)
+
+    } catch (err) {
+      console.log(err)
+      setError(true)
+
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const renderThings = ()=>{
+    if (loading){ return(
+      <Segment>
+      <Dimmer active>
+        <Loader />
+      </Dimmer>
+    </Segment>
+    )
+   }
+    if (error){ return <p>Error</p> }
+    return things.map((t)=>{
+      return (
+        <div>
+          {t.name}
+        </div>
+      )
+    })
+  }
+
+
+  return (
+    <>
+    <Header>Things</Header>
+    {renderThings()}
+    </>
+  )
 }
